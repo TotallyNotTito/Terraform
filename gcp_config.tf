95bca02960eaac7f0da9aa837e5f287f48cda63b
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-11-14 08:55:27 -0800

    Update gcp_config.tf

--- gcp_config.tf
+++ gcp_config.tf
@@ -1,7 +1,7 @@
 // Configure the Google Cloud provider
 provider "google" {
  credentials = file("tf-lab.json")
- project     = "cloud-reinhart-tkral"
+ project     = "<FMI>"
  region      = "us-west1"
 }
 
@@ -10,18 +10,31 @@ resource "google_compute_instance" "default" {
  name         = "tf-lab-vm"
  machine_type = "f1-micro"
  zone         = "us-west1-b"
-
+ metadata = {
+    ssh-keys = "<FMI>:${file("~/.ssh/id_ed25519.pub")}"
+ }
+ metadata_startup_script = "${file("install.sh")}"
+ tags = ["http-server"]
  boot_disk {
    initialize_params {
      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20221018"
    }
  }
+}
 
  network_interface {
    network = "default"
- }
+   access_config {
+     nat_ip = google_compute_address.static.address
+   }
 }
 
+// A single IPv4 address
 resource "google_compute_address" "static" {
-    name = "ipv4-address"
+  name = "ipv4-address"
+}
+
+// A variable for extracting the external IP address of the instance
+output "ip" {
+ value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
