7705216ea9f651ea20a898f4fb6d73e4ce96de8d
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-11-13 22:16:29 -0800

    Create gcp_config.tf

---
+++ gcp_config.tf
@@ -0,0 +1,23 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = file("tf-lab.json")
+ project     = "cloud-reinhart-tkral"
+ region      = "us-west1"
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ name         = "tf-lab-vm"
+ machine_type = "f1-micro"
+ zone         = "us-west1-b"
+
+ boot_disk {
+   initialize_params {
+     image = "ubuntu-os-cloud/ubuntu-2004-focal-v20221018"
+   }
+ }
+
+ network_interface {
+   network = "default"
+ }
+}
